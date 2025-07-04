e5964f2165ac1feea4a879963dcffb9ba1099701
LautaroGoyo <lautarogoyoaga@gmail.com>
2025-07-04 20:40:45 -0300

    Creacion de CRUD Cliente y TipoPropiedad

--- APP-Inmobiliaria/backend/src/app.ts
+++ APP-Inmobiliaria/backend/src/app.ts
@@ -1,1 +1,17 @@
-console.log('go ts')
+import express from 'express'
+import { clienteRouter } from './cliente/cliente.routes.js'
+import { tipopropiedadRouter } from './tipopropiedad/tipopropiedad.routes.js'
+
+const app = express()
+app.use(express.json())
+
+app.use('/api/clientes', clienteRouter)
+app.use('/api/tipopropiedades', tipopropiedadRouter)
+
+app.use((_, res) => {
+  return res.status(404).send({ message: 'Resource not found' })
+})
+
+app.listen(3000, () => {
+  console.log('Server runnning on http://localhost:3000/')
+})
