f8e1c9ab7c1e3daa0afad94e5ca611a5149cdd69
Tomás <tomassuarez2002@gmail.com>
2025-09-04 17:23:07 -0300

    Implementación de Router y mejora de Rutas

--- APP-Inmobiliaria/frontend/src/App.tsx
+++ APP-Inmobiliaria/frontend/src/App.tsx
@@ -1,21 +1,7 @@
-import { Route ,Routes} from 'react-router-dom'
-import Home from './pages/Home.tsx';
-import Contact from './pages/Contact.tsx';
-import Rent from './pages/Rent.tsx';
-import Visit from './pages/Visit.tsx';
-import Reserv from './pages/Reserv.tsx';
-import Login from './pages/Login.tsx';
+import { AppRouter } from './routers/AppRouter.tsx';
 
 export default function App() {
   return (
-    <Routes>
-      <Route path='/' element={<Home/>}/>
-      <Route path='/Contact' element={<Contact/>}/>
-      <Route path='/Rent' element={<Rent/>}/>
-      <Route path='/Rent/Visit' element={<Visit/>}/>
-      <Route path='/Rent/Reserv' element={<Reserv/>}/>
-      <Route path='/Login' element={<Login/>}/>
-      
-    </Routes>
+    <AppRouter/>
   )
 }
---
+++ APP-Inmobiliaria/frontend/src/pages/index.tsx
@@ -0,0 +1,6 @@
+export {default as Login} from "./Login.tsx";
+export {default as Home} from "./Home";
+export {default as Rent} from "./Rent";
+export {default as Reserv} from "./Reserv";
+export {default as Visit} from "./Visit";
+export {default as Contact} from "./Contact";
