785d90cfbd5313103ce121c4c8545ef3de6f3fc9
lautarogoyo <lgoyoaga@frro.utn.edu.ar>
2025-09-16 16:25:36 -0300

    Cambios en card

--- APP-Inmobiliaria/frontend/src/components/Card.tsx
+++ APP-Inmobiliaria/frontend/src/components/Card.tsx
@@ -16,17 +16,16 @@ export type Propiedades = {
   tiposPropiedad: TipoPropiedad[]
 }
 
-export default function Card({ id, direccion, precio, estado, inmobiliaria, tiposPropiedad, imagen }: Propiedades) {
+export default function Card({direccion, precio, estado, inmobiliaria, tiposPropiedad, imagen }: Propiedades) {
   return (
     <div className="bg-white group relative p-3 rounded-2xl shadow-md hover:shadow-lg transition-shadow duration-300">
       <img
         src={imagen ? imagen : 'src/assets/DeptoPrueba.jpg'}
         alt={direccion}
-        className="w-full aspect-auto object-cover bg-white group-hover:opacity-80 rounded-xl mb-2"
+        className="w-full aspect-auto object-cover bg-white group-hover:opacity-80 rounded-xl mb-2 hover:cursor-pointer"
       />
-      <div className="mb-2 flex justify-between items-center">
-        <span className="text-xs text-gray-400">ID: {id}</span>
-        <span className={`px-2 py-1 rounded text-xs font-semibold ${estado === 'disponible' ? 'bg-green-100 text-green-700' : 'bg-red-100 text-red-700'}`}>{estado}</span>
+      <div className="m-3 flex justify-end">
+        <span className={`px-2 py-1 rounded text-xs font-semibold ${estado === 'disponible' ? 'bg-green-100 text-green-700' : 'bg-red-100 text-red-700'}`}>{estado.toUpperCase()}</span>
       </div>
       <h3 className="text-xl text-gray-700 font-bold mb-1">{direccion}</h3>
       <p className="text-sm text-gray-500 mb-1">Inmobiliaria: {inmobiliaria ? inmobiliaria.nombre : 'Sin inmobiliaria'}</p>
